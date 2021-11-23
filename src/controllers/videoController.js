ea17579536c135edeb6d75ee184a991826971e17
ApostleB <jbu1391@gmail.com>
2021-11-24 02:10:43 +0900

    pug mixin / pug재사용

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,7 +3,32 @@ const fakeUser = {
     loggedIn: true
 };
 export const trendingVideos = (req, res) => {
-    const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const videos = [
+        {
+            title: "First Video",
+            rating:5,
+            comments:2,
+            createdAt: "2 minutes ago",
+            views:59,
+            id:1,
+        },
+        {
+            title: "Second Video",
+            rating:5,
+            comments:2,
+            createdAt: "3 minutes ago",
+            views:59,
+            id:2,
+        },
+        {
+            title: "Third Video",
+            rating:5,
+            comments:2,
+            createdAt: "4minutes ago",
+            views:98,
+            id:3,
+        }
+    ];
     return res.render("home", { pageTitle : "Home", videos  });
 }
 
