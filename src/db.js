b6b88cf2f9d7797399a8464f413c3636bd4e435e
ApostleB <jbu1391@gmail.com>
2022-01-12 22:00:40 +0900

    bug find

--- src/db.js
+++ src/db.js
@@ -11,13 +11,24 @@ useNewUrlParser, useUnifiedTopology를 사용한다고 하는데
 */
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.DB_URL);
-// mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify:false,
-//     useCreateIndex: true,
-// });
+// mongoose.connect(process.env.DB_URL);
+// // mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
+// //     useNewUrlParser: true,
+// //     useUnifiedTopology: true,
+// //     useFindAndModify:false,
+// //     useCreateIndex: true,
+// // });
+
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+
+
+
 
 const db = mongoose.connection
 db.on("error", (err) => console.log("☣️🚫DB Error", err));  //db.on은 여러번 발생 가능
