b0489b5b82bafe19d68bb6d0ba38f80d63fc5fed
ApostleB <jbu1391@gmail.com>
2022-01-12 21:47:06 +0900

    PORT env variable

--- src/init.js
+++ src/init.js
@@ -9,7 +9,7 @@ import "./models/Video";
 import "./models/User";
 import "./models/Comment";
 import app from "./server";
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const handleListening = () =>
   console.log(`Server Listenting on port http://localhost:${PORT}`);
