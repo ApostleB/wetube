beff5cbeb1487cf31fa0141bd058674bcc431d9e
ApostleB <jbu1391@gmail.com>
2022-01-12 22:05:49 +0900

    bugfind

--- src/db.js
+++ src/db.js
@@ -22,8 +22,6 @@ import mongoose from "mongoose";
 mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: false,
 });
 
 
